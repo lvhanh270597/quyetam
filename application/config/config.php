391743b3b91024fa2092201cea75dccb4582d750
root <lvhanh.270597@gmail.com>
2018-10-20 06:17:53 +0000

    add

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] =  'http://together.easyhere.cf/quyetam/';
+$config['base_url'] =  'https://together.easyhere.cf/';
 
 /*
 |--------------------------------------------------------------------------
