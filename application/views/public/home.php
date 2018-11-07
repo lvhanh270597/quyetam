bc64926510285decbf88abc0e0834ede4f449f94
lvhanh270597 <lvhanh.270597@gmail.com>
2018-11-07 16:58:37 +0700

    add

--- application/views/public/home.php
+++ application/views/public/home.php
@@ -53,93 +53,7 @@
                             </div>
                             <!-- Nav tabs -->
                             <!-- Tab panels -->
-                            <div class="tab-content">
-                                <!--Panel 1-->
-                                <div class="tab-pane fade in show active" id="panel555" role="tabpanel">
-                                    <!-- Nav tabs -->
-                                    <div class="row">
-                                    <div class="card-body card-body-cascade">
-                                                        <!--Card-->
-                                                        
-                                        <div class="card card-cascade narrower">
-                                            <!-- Card content -->
-                                            
-                                            <div class="row">                                                
-                                            <?php
-                                                if ($trips){
-                                                    foreach ($trips as $trip){
-                                                        $owner = $this->user_ml->get_by_primary($trip['owner']);
-                                                        $guess = $this->user_ml->get_by_primary($trip['guess']);
-                                                        $place = $this->place_ml->get_by_primary($trip['finish_to']);                                    
-                                                        $free = '';
-                                                        if ($trip['price'] == 0){
-                                                            $free = '<span class="badge badge-info mb-2">free</span>';
-                                                        }
-                                                        else{
-                                                            $free = '<span class="badge badge-info mb-2">'.$trip['price'].'đ</span>';
-                                                        }
-                                                        $empty = '';
-                                                        if ($trip['guess'] == null){
-                                                            $empty = '<span class="badge badge-primary mb-2">còn trống</span></br>';
-                                                        }                              
-                                                        else{
-                                                            $empty .= '<span class="badge badge-danger mb-2">'.$guess['full_name'].'</span>'.get_color_status(get_status($trip['guess'])).'</br>';
-                                                        }  
-                                                        echo '<div class="col-lg-3 col-md-6 mb-4" >
-                                                        <!--Card-->
-                                                        <div class="card card-ecommerce">
-                                                            <!--Card image-->
-                                                            <div class="view overlay">
-                                                                <img src="'.base_url('assets/images/uploads/places/'.$place['id'].'/'.$place['image']).'" class="img-fluid" alt="" id="dm">
-                                                                <a href="'.site_url('trip/detail/'.$trip['id']).'">
-                                                                    <div class="mask rgba-white-slight waves-effect waves-light">                                            
-                                                                    </div>                                            
-                                                                </a>
-                                                            </div>
-                                                            <!--Card image-->                                                            
-
-                                                            <!--Card content-->
-                                                            <div class="card-body">                                    
-                                                                <!--Category & Title-->
-                                                                <h6 class="card-title mb-1"><strong><a href="'.site_url('trip/detail/'.$trip['id']).'" class="dark-grey-text">'.$places[$trip['start_from']].'<i class="fa fa-mail-forward" aria-hidden="true"></i> '.$places[$trip['finish_to']].'</a> </strong></h6>
-                                                                <span class="badge badge-success mb-2">'.$owner['full_name'].'</span>'.get_color_status(get_status($owner['username'])).' </br>
-                                                                '.$empty.'
-                                                                '.$free.'                                                                
-                                                                <!--Card footer-->
-                                                                <div class="card-footer pb-0">
-                                                                    <div class="row mb-0">                                                            
-                                                                        <span class="float-left"><strong> '.after($trip['timestart']).'</strong></span>
-                                                                    </div>                                                                    
-                                                                </div>
-                                                            </div>
-                                                            <!--Card content-->
-
-                                                        </div>
-                                                        <!--Card-->
-
-                                                    </div>';
-                                                    }
-                                                }
-                                                else{
-                                                    echo '
-                                                    <div class="col-md-12">
-                                                    <h5>CHƯA CÓ CHUYẾN ĐI NÀO</h5>
-                                                    </div>
-                                                    ';      
-                                                }
-                                            ?>
-
-                                                <!--Grid column-->
-                                                
-                                                <!--Grid column-->                        
-
-                                                </div>                                            
-                                        </div>
-                                    </div>
-                                    </div>
-                                    <!-- Nav tabs -->
-                                </div>
-                                <!--/.Panel 1-->
+                            <div class="tab-content">                                
                                 <!--Panel 2-->
                                 <div class="tab-pane fade" id="panel666" role="tabpanel">
                                     <!-- Card content -->
@@ -241,6 +155,92 @@
 
                                 </div>
                                 <!--/.Panel 2-->
+                                <!--Panel 1-->
+                                <div class="tab-pane fade in show active" id="panel555" role="tabpanel">
+                                    <!-- Nav tabs -->
+                                    <div class="row">
+                                    <div class="card-body card-body-cascade">
+                                                        <!--Card-->
+                                                        
+                                        <div class="card card-cascade narrower">
+                                            <!-- Card content -->
+                                            
+                                            <div class="row">                                                
+                                            <?php
+                                                if ($trips){
+                                                    foreach ($trips as $trip){
+                                                        $owner = $this->user_ml->get_by_primary($trip['owner']);
+                                                        $guess = $this->user_ml->get_by_primary($trip['guess']);
+                                                        $place = $this->place_ml->get_by_primary($trip['finish_to']);                                    
+                                                        $free = '';
+                                                        if ($trip['price'] == 0){
+                                                            $free = '<span class="badge badge-info mb-2">free</span>';
+                                                        }
+                                                        else{
+                                                            $free = '<span class="badge badge-info mb-2">'.$trip['price'].'đ</span>';
+                                                        }
+                                                        $empty = '';
+                                                        if ($trip['guess'] == null){
+                                                            $empty = '<span class="badge badge-primary mb-2">còn trống</span></br>';
+                                                        }                              
+                                                        else{
+                                                            $empty .= '<span class="badge badge-danger mb-2">'.$guess['full_name'].'</span>'.get_color_status(get_status($trip['guess'])).'</br>';
+                                                        }  
+                                                        echo '<div class="col-lg-3 col-md-6 mb-4" >
+                                                        <!--Card-->
+                                                        <div class="card card-ecommerce">
+                                                            <!--Card image-->
+                                                            <div class="view overlay">
+                                                                <img src="'.base_url('assets/images/uploads/places/'.$place['id'].'/'.$place['image']).'" class="img-fluid" alt="" id="dm">
+                                                                <a href="'.site_url('trip/detail/'.$trip['id']).'">
+                                                                    <div class="mask rgba-white-slight waves-effect waves-light">                                            
+                                                                    </div>                                            
+                                                                </a>
+                                                            </div>
+                                                            <!--Card image-->                                                            
+
+                                                            <!--Card content-->
+                                                            <div class="card-body">                                    
+                                                                <!--Category & Title-->
+                                                                <h6 class="card-title mb-1"><strong><a href="'.site_url('trip/detail/'.$trip['id']).'" class="dark-grey-text">'.$places[$trip['start_from']].'<i class="fa fa-mail-forward" aria-hidden="true"></i> '.$places[$trip['finish_to']].'</a> </strong></h6>
+                                                                <span class="badge badge-success mb-2">'.$owner['full_name'].'</span>'.get_color_status(get_status($owner['username'])).' </br>
+                                                                '.$empty.'
+                                                                '.$free.'                                                                
+                                                                <!--Card footer-->
+                                                                <div class="card-footer pb-0">
+                                                                    <div class="row mb-0">                                                            
+                                                                        <span class="float-left"><strong> '.after($trip['timestart']).'</strong></span>
+                                                                    </div>                                                                    
+                                                                </div>
+                                                            </div>
+                                                            <!--Card content-->
+
+                                                        </div>
+                                                        <!--Card-->
+
+                                                    </div>';
+                                                    }
+                                                }
+                                                else{
+                                                    echo '
+                                                    <div class="col-md-12">
+                                                    <h5>CHƯA CÓ CHUYẾN ĐI NÀO</h5>
+                                                    </div>
+                                                    ';      
+                                                }
+                                            ?>
+
+                                                <!--Grid column-->
+                                                
+                                                <!--Grid column-->                        
+
+                                                </div>                                            
+                                        </div>
+                                    </div>
+                                    </div>
+                                    <!-- Nav tabs -->
+                                </div>
+                                <!--/.Panel 1-->
                             </div>
                             <!-- Tab panels -->
                         </div>
