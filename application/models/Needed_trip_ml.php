74d45d65d9144d81224b0e28bcf1449bda78cb46
lvhanh270597 <lvhanh.270597@gmail.com>
2018-11-22 22:10:52 +0700

    fix

--- application/models/Needed_trip_ml.php
+++ application/models/Needed_trip_ml.php
@@ -166,7 +166,7 @@ class Needed_trip_ml extends Trip_template
 		$res['finish_to'] = $dataset->result_array();
 		$dataset = $this->db->query('SELECT HOUR(timestart) as hour, COUNT(*) as cnt FROM '.$this->db_table.' GROUP BY HOUR(timestart)');
 		$res['hour_cnt'] = $dataset->result_array();
-		$dataset = $this->db->query("SELECT * FROM ".$this->db_table." WHERE (trip_id = 0) and CAST(created AS DATE) = ".'"'.$target_date.'"');		
+		$dataset = $this->db->query("SELECT * FROM ".$this->db_table." WHERE (trip_id = 0) and CAST(timestart AS DATE) = ".'"'.$target_date.'"');		
 		$res['success_ask'] = $res['cnt'] - $dataset->num_rows();
 
 		$dataset = $this->db->query("SELECT * FROM ".$this->db_table);		
