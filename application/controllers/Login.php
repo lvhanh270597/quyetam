34587ed8a2e4a164903d223f02aa426ca6b4f196
lvhanh270597 <lvhanh.270597@gmail.com>
2018-12-05 15:55:41 +0700

    dad

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
     
     public function __construct(){
-        parent::__construct();                
+        parent::__construct();     
     }       
     
     public function index(){                         
@@ -39,6 +39,7 @@ class Login extends CI_Controller {
     }
 
     public function register(){   
+        redirect(base_url()); 
         if ($this->session->userdata('user_logged')){
            redirect(base_url()); 
         }   
