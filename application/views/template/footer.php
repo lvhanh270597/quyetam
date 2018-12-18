618dd0a54d59bd2f24b49b559207f8697dee1f62
lvhanh270597 <lvhanh.270597@gmail.com>
2018-12-19 02:05:09 +0700

    add

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -109,11 +109,5 @@
     
     ?>    
 
-    
-<script>
-    $(document).ready(function(){ 
-	$('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();
-    }); 
-</script>
     </body>
 </html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,10 +7,8 @@
 
 <title>Easy Here</title>
 
-<script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js') ?>"> </script>
-
 <!-- Font Awesome -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="<?= base_url('assets/font/font-awesome.min.css') ?>">
 
 <!-- Bootstrap core CSS -->
 
